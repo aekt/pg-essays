22ce9a16bf2d2f4d05efa8a1e375964e82f2aaff
aekt <yusan-23@hotmail.com>
2018-07-05 05:03:05 +0800

    Basic prototype
    Added functions:
    - fetching articles
    - pick-and-read UI
    - using own proxy on Heroku
    
    But there are some issues in extracting main content from articles,
    perhaps a serious parsing is needed.

--- app.js
+++ app.js
@@ -1,33 +1,65 @@
+'use strict';
+
+let proxy = 'https://pristine-voyageurs-98160.herokuapp.com/';
+
+function extract(str, tag) {
+  let reStr = '<' + tag + '[^>]*?>(.|\n)*?</' + tag + '>';
+  let reTag = new RegExp(reStr, 'g');
+  let reInner = new RegExp('>(.|\n)*<');
+  let matches = str.match(reTag);
+  return matches.map(s => s.match(reInner)[0].slice(1, -1));
+}
+
 let vm = new Vue({
   el: '#app',
+  
   data: {
-    message: 'Loading...',
+    mode: 'loading',
     items: [],
+    current: 0,
+    content: '',
   },
+
   created: function() {
-    let proxy = 'https://cors-anywhere.herokuapp.com/';
     let url = 'http://www.aaronsw.com/2002/feeds/pgessays.rss';
     fetch(proxy + url)
     .then(resp => resp.text())
-    .then(this.listing);
-  },
-  methods: {
-    listing: function(text) {
-      function extract(str, tag) {
-        let re = new RegExp('<' + tag + '>(.|\n)*?</' + tag + '>', 'g');
-        let list = str.match(re);
-        let tl = tag.length;
-        return list.map(s => s.slice(tl+2, -tl-3));
-      }
+    .then(text => {
       let list = extract(text, 'item');
       for (let item of list) {
         this.items.push({
           title: extract(item, 'title')[0],
           link: extract(item, 'link')[0],
-          read: false,
+          archived: false,
         });
       }
-      this.message = '';
-    }
+      this.mode = 'picking';
+    })
+    .catch(() => {
+      this.mode = 'failing';
+    });
+  },
+  
+  methods: {
+    read: function(index) {
+      this.current = index;
+      this.mode = 'loading';
+      fetch(proxy + this.items[index].link)
+      .then(resp => resp.text())
+      .then(text => {
+        this.content = extract(text, 'font')[0];
+        this.mode = 'reading';
+      })
+      .catch(() => {
+        this.mode = 'failing';
+      });
+    },
+    pick: function({mark = false}) {
+      if (mark) {
+        let archived = this.items[this.current].archived;
+        this.items[this.current].archived = !archived;
+      }
+      this.mode = 'picking';
+    },
   },
 });
